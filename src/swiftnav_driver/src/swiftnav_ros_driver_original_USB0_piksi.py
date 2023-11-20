1396d282755edf07d7042ebabefcf026ae7743dd
gonultasbu <gonultasbu@users.noreply.github.com>
2023-11-20 11:27:47 -0600

    refactoring

--- src/swiftnav_driver/src/swiftnav_ros_driver_original_USB0_piksi.py
+++ src/swiftnav_driver/src/swiftnav_ros_driver_original_USB0_piksi.py
@@ -2,11 +2,16 @@
 
 from sbp.client.drivers.pyserial_driver import PySerialDriver
 from sbp.client import Handler, Framer
-from sbp.navigation import SBP_MSG_BASELINE_NED, MsgBaselineNED, SBP_MSG_POS_LLH, MsgPosLLH
+from sbp.navigation import (
+    SBP_MSG_BASELINE_NED,
+    MsgBaselineNED,
+    SBP_MSG_POS_LLH,
+    MsgPosLLH,
+)
 from sbp.imu import SBP_MSG_IMU_RAW, MsgImuRaw, SBP_MSG_IMU_AUX, MsgImuAux
 from sbp.mag import SBP_MSG_MAG_RAW, MsgMagRaw
 import argparse
-import pdb;
+import pdb
 
 import rospy
 from geometry_msgs.msg import Point, Accel, Vector3
@@ -15,15 +20,21 @@ from std_msgs.msg import Float64MultiArray, Float64, UInt8
 from swiftnav_driver.msg import s16_Vec3, imu, imu_aux, imu_scaled, gps_loc
 import numpy as np
 
-rospy.init_node('piksi')
-pub_llh = rospy.Publisher(rospy.get_name() + '/pos_llh', gps_loc, queue_size=10)
-pub_enu_point = rospy.Publisher(rospy.get_name() + '/enu_pose_best_fix', Point, queue_size=10)
-pub_enu = rospy.Publisher(rospy.get_name() + '/enu_pose', gps_loc, queue_size=10)
-pub_ned = rospy.Publisher(rospy.get_name() + '/ned_pose_best_fix', gps_loc, queue_size=10)
-pub_imu_raw = rospy.Publisher(rospy.get_name() + '/imu_raw', imu, queue_size=10)
-pub_imu_scaled = rospy.Publisher(rospy.get_name() + '/imu_scaled', imu_scaled, queue_size=10)
-pub_imu_aux = rospy.Publisher(rospy.get_name() + '/imu_aux', imu_aux, queue_size=10)
-pub_mag_raw = rospy.Publisher(rospy.get_name() + '/mag_raw', s16_Vec3, queue_size=10)
+rospy.init_node("piksi")
+pub_llh = rospy.Publisher(rospy.get_name() + "/pos_llh", gps_loc, queue_size=10)
+pub_enu_point = rospy.Publisher(
+    rospy.get_name() + "/enu_pose_best_fix", Point, queue_size=10
+)
+pub_enu = rospy.Publisher(rospy.get_name() + "/enu_pose", gps_loc, queue_size=10)
+pub_ned = rospy.Publisher(
+    rospy.get_name() + "/ned_pose_best_fix", gps_loc, queue_size=10
+)
+pub_imu_raw = rospy.Publisher(rospy.get_name() + "/imu_raw", imu, queue_size=10)
+pub_imu_scaled = rospy.Publisher(
+    rospy.get_name() + "/imu_scaled", imu_scaled, queue_size=10
+)
+pub_imu_aux = rospy.Publisher(rospy.get_name() + "/imu_aux", imu_aux, queue_size=10)
+pub_mag_raw = rospy.Publisher(rospy.get_name() + "/mag_raw", s16_Vec3, queue_size=10)
 
 g_value = 9.81
 imu_scale_flag = 0
@@ -31,12 +42,21 @@ imu_res = np.iinfo(np.int16).max
 acc_scale = 0.00
 gyr_scale = 0.00
 
-def cb_SBP_MSG_POS_LLH(msg_llh, **source):
-    # rate = rospy.Rate(10)
-    msg = MsgPosLLH(msg_llh)
-    # rospy.loginfo(msg)
 
-    llhCoods  = gps_loc()
+def cb_SBP_MSG_POS_LLH(msg_llh, **source):
+    """
+    Callback function for SBP_MSG_POS_LLH messages.
+
+    Args:
+        msg_llh: SBP_MSG_POS_LLH message object
+        source: Additional source information
+
+    Returns:
+        None
+    """
+    msg = MsgPosLLH(msg_llh)
+
+    llhCoods = gps_loc()
     llhCoods.loc.x = msg.lat
     llhCoods.loc.y = msg.lon
     llhCoods.loc.z = msg.height
@@ -44,14 +64,19 @@ def cb_SBP_MSG_POS_LLH(msg_llh, **source):
 
     pub_llh.publish(llhCoods)
 
-    # rospy.loginfo('lat lon message recieved')
-    # # rospy.loginfo(llhCoods)
-    # rate.sleep()
 
 def cb_SBP_MSG_BASELINE_NED(msg_ned, **source):
-    # rate = rospy.Rate(10)
+    """
+    Callback function for SBP_MSG_BASELINE_NED messages.
+
+    Args:
+        msg_ned: SBP_MSG_BASELINE_NED message object
+        source: Additional source information
+
+    Returns:
+        None
+    """
     msg = MsgBaselineNED(msg_ned)
-    # rospy.loginfo(msg)
 
     rtk_fix_enu_point = Point()
     rtk_fix_enu_point.x = msg.e * 1e-3
@@ -73,18 +98,23 @@ def cb_SBP_MSG_BASELINE_NED(msg_ned, **source):
     rtk_fix_ned.tow = msg.tow
     pub_ned.publish(rtk_fix_ned)
 
-    # rospy.loginfo('enu message recieved')
-    # # rospy.loginfo(rtk_fix)
-    # rate.sleep()
 
 def cb_SBP_MSG_IMU_RAW(msg_imu_raw, **source):
+    """
+    Callback function for SBP_MSG_IMU_RAW messages.
+
+    Args:
+        msg_imu_raw: SBP_MSG_IMU_RAW message object
+        source: Additional source information
+
+    Returns:
+        None
+    """
     global imu_scale_flag
     global acc_scale
     global gyr_scale
 
-    # rate = rospy.Rate(100)
     msg = MsgImuRaw(msg_imu_raw)
-    # rospy.loginfo(msg)
 
     imu_raw_msg = imu()
     imu_raw_msg.acc.x = msg.acc_x
@@ -97,38 +127,36 @@ def cb_SBP_MSG_IMU_RAW(msg_imu_raw, **source):
     pub_imu_raw.publish(imu_raw_msg)
 
     if imu_scale_flag == 0:
-        imu_scale_flag = rospy.get_param(rospy.get_name()+'/imu_scale_flag')
+        imu_scale_flag = rospy.get_param(rospy.get_name() + "/imu_scale_flag")
         if imu_scale_flag == 1:
-            rospy.loginfo('IMU scale found')
-            acc_scale = rospy.get_param(rospy.get_name()+'/acc_scale')
-            rospy.loginfo('acc scale = ' + str(acc_scale))
-            rospy.loginfo('acc range = ' + str(rospy.get_param(rospy.get_name()+'/acc_range')))
-            gyr_scale = rospy.get_param(rospy.get_name()+'/gyr_scale')
-            rospy.loginfo('gyr scale = ' + str(gyr_scale))
-            rospy.loginfo('gyr range = ' + str(rospy.get_param(rospy.get_name()+'/gyr_range')))
+            acc_scale = rospy.get_param(rospy.get_name() + "/acc_scale")
+            gyr_scale = rospy.get_param(rospy.get_name() + "/gyr_scale")
 
     imu_scaled_msg = imu_scaled()
     if imu_scale_flag == 1:
-        imu_scaled_msg.acc.x = imu_raw_msg.acc.x*acc_scale
-        imu_scaled_msg.acc.y = imu_raw_msg.acc.y*acc_scale
-        imu_scaled_msg.acc.z = imu_raw_msg.acc.z*acc_scale
-        imu_scaled_msg.gyr.x = imu_raw_msg.gyr.x*gyr_scale
-        imu_scaled_msg.gyr.y = imu_raw_msg.gyr.y*gyr_scale
-        imu_scaled_msg.gyr.z = imu_raw_msg.gyr.z*gyr_scale
+        imu_scaled_msg.acc.x = imu_raw_msg.acc.x * acc_scale
+        imu_scaled_msg.acc.y = imu_raw_msg.acc.y * acc_scale
+        imu_scaled_msg.acc.z = imu_raw_msg.acc.z * acc_scale
+        imu_scaled_msg.gyr.x = imu_raw_msg.gyr.x * gyr_scale
+        imu_scaled_msg.gyr.y = imu_raw_msg.gyr.y * gyr_scale
+        imu_scaled_msg.gyr.z = imu_raw_msg.gyr.z * gyr_scale
         imu_scaled_msg.tow = imu_raw_msg.tow
-    else:
-        rospy.loginfo('IMU scale not available')
 
     pub_imu_scaled.publish(imu_scaled_msg)
 
-    # rospy.loginfo('imu message recieved')
-    # # rospy.loginfo(imu_raw_msg)
-    # rate.sleep()
 
 def cb_SBP_MSG_IMU_AUX(msg_imu_aux, **source):
-    # rate = rospy.Rate(100)
+    """
+    Callback function for SBP_MSG_IMU_AUX messages.
+
+    Args:
+        msg_imu_aux: SBP_MSG_IMU_AUX message object
+        source: Additional source information
+
+    Returns:
+        None
+    """
     msg = MsgImuAux(msg_imu_aux)
-    # rospy.loginfo(msg)
 
     imu_aux_msg = imu_aux()
     imu_aux_msg.imu_conf = msg.imu_conf
@@ -136,20 +164,20 @@ def cb_SBP_MSG_IMU_AUX(msg_imu_aux, **source):
     imu_aux_msg.imu_type = msg.imu_type
 
     acc_conf = msg.imu_conf & 7
-    acc_range = g_value*(2**(acc_conf+1))
-    acc_scale = acc_range/imu_res
+    acc_range = g_value * (2 ** (acc_conf + 1))
+    acc_scale = acc_range / imu_res
 
     gyr_conf = msg.imu_conf >> 4
-    gyr_range = 2000.00/(2**(gyr_conf))
-    gyr_scale = gyr_range/imu_res
+    gyr_range = 2000.00 / (2 ** (gyr_conf))
+    gyr_scale = gyr_range / imu_res
 
-    rospy.set_param(rospy.get_name()+'/imu_scale_flag',1)
-    rospy.set_param(rospy.get_name()+'/acc_conf',acc_conf)
-    rospy.set_param(rospy.get_name()+'/acc_range',acc_range)
-    rospy.set_param(rospy.get_name()+'/acc_scale',acc_scale)
-    rospy.set_param(rospy.get_name()+'/gyr_conf',gyr_conf)
-    rospy.set_param(rospy.get_name()+'/gyr_range',gyr_range)
-    rospy.set_param(rospy.get_name()+'/gyr_scale',gyr_scale)
+    rospy.set_param(rospy.get_name() + "/imu_scale_flag", 1)
+    rospy.set_param(rospy.get_name() + "/acc_conf", acc_conf)
+    rospy.set_param(rospy.get_name() + "/acc_range", acc_range)
+    rospy.set_param(rospy.get_name() + "/acc_scale", acc_scale)
+    rospy.set_param(rospy.get_name() + "/gyr_conf", gyr_conf)
+    rospy.set_param(rospy.get_name() + "/gyr_range", gyr_range)
+    rospy.set_param(rospy.get_name() + "/gyr_scale", gyr_scale)
 
     pub_imu_aux.publish(imu_aux_msg)
 
@@ -162,6 +190,7 @@ def cb_SBP_MSG_IMU_AUX(msg_imu_aux, **source):
     # rospy.loginfo(gyr_range)
     # rospy.loginfo(gyr_scale)
     # rate.sleep()
+
 
 def main():
     # parser = argparse.ArgumentParser()
@@ -177,21 +206,20 @@ def main():
 
     # threads = []
 
-
-    driver = PySerialDriver('/dev/ttyUSB0', baud=230400)
+    driver = PySerialDriver("/dev/ttyUSB0", baud=230400)
     framer = Framer(driver.read, None, verbose=True)
     source = Handler(framer)
 
-    rospy.set_param(rospy.get_name()+'/imu_scale_flag',0)
+    rospy.set_param(rospy.get_name() + "/imu_scale_flag", 0)
 
-
-    source.add_callback(cb_SBP_MSG_POS_LLH,msg_type=SBP_MSG_POS_LLH)
-    source.add_callback(cb_SBP_MSG_BASELINE_NED,msg_type=SBP_MSG_BASELINE_NED)
-    source.add_callback(cb_SBP_MSG_IMU_RAW,msg_type=SBP_MSG_IMU_RAW)
-    source.add_callback(cb_SBP_MSG_IMU_AUX,msg_type=SBP_MSG_IMU_AUX)
+    source.add_callback(cb_SBP_MSG_POS_LLH, msg_type=SBP_MSG_POS_LLH)
+    source.add_callback(cb_SBP_MSG_BASELINE_NED, msg_type=SBP_MSG_BASELINE_NED)
+    source.add_callback(cb_SBP_MSG_IMU_RAW, msg_type=SBP_MSG_IMU_RAW)
+    source.add_callback(cb_SBP_MSG_IMU_AUX, msg_type=SBP_MSG_IMU_AUX)
     source.start()
 
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
--- src/swiftnav_driver/src/swiftnav_ros_driver_piksi.py
+++ src/swiftnav_driver/src/swiftnav_ros_driver_piksi.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from sbp.client.drivers.pyserial_driver import PySerialDriver
 from sbp.client import Handler, Framer
-from sbp.navigation import SBP_MSG_BASELINE_NED, MsgBaselineNED, SBP_MSG_POS_LLH, MsgPosLLH
+from sbp.navigation import (
+    SBP_MSG_BASELINE_NED,
+    MsgBaselineNED,
+    SBP_MSG_POS_LLH,
+    MsgPosLLH,
+)
 from sbp.imu import SBP_MSG_IMU_RAW, MsgImuRaw, SBP_MSG_IMU_AUX, MsgImuAux
 from sbp.mag import SBP_MSG_MAG_RAW, MsgMagRaw
 import argparse
-import pdb;
+import pdb
 
 import rospy
 from geometry_msgs.msg import Point, Accel, Vector3
@@ -14,15 +19,21 @@ from std_msgs.msg import Float64MultiArray, Float64, UInt8
 from swiftnav_driver.msg import s16_Vec3, imu, imu_aux, imu_scaled, gps_loc
 import numpy as np
 
-rospy.init_node('piksi')
-pub_llh = rospy.Publisher(rospy.get_name() + '/pos_llh', gps_loc, queue_size=10)
-pub_enu_point = rospy.Publisher(rospy.get_name() + '/enu_pose_best_fix', Point, queue_size=10)
-pub_enu = rospy.Publisher(rospy.get_name() + '/enu_pose', gps_loc, queue_size=10)
-pub_ned = rospy.Publisher(rospy.get_name() + '/ned_pose_best_fix', gps_loc, queue_size=10)
-pub_imu_raw = rospy.Publisher(rospy.get_name() + '/imu_raw', imu, queue_size=10)
-pub_imu_scaled = rospy.Publisher(rospy.get_name() + '/imu_scaled', imu_scaled, queue_size=10)
-pub_imu_aux = rospy.Publisher(rospy.get_name() + '/imu_aux', imu_aux, queue_size=10)
-pub_mag_raw = rospy.Publisher(rospy.get_name() + '/mag_raw', s16_Vec3, queue_size=10)
+rospy.init_node("piksi")
+pub_llh = rospy.Publisher(rospy.get_name() + "/pos_llh", gps_loc, queue_size=10)
+pub_enu_point = rospy.Publisher(
+    rospy.get_name() + "/enu_pose_best_fix", Point, queue_size=10
+)
+pub_enu = rospy.Publisher(rospy.get_name() + "/enu_pose", gps_loc, queue_size=10)
+pub_ned = rospy.Publisher(
+    rospy.get_name() + "/ned_pose_best_fix", gps_loc, queue_size=10
+)
+pub_imu_raw = rospy.Publisher(rospy.get_name() + "/imu_raw", imu, queue_size=10)
+pub_imu_scaled = rospy.Publisher(
+    rospy.get_name() + "/imu_scaled", imu_scaled, queue_size=10
+)
+pub_imu_aux = rospy.Publisher(rospy.get_name() + "/imu_aux", imu_aux, queue_size=10)
+pub_mag_raw = rospy.Publisher(rospy.get_name() + "/mag_raw", s16_Vec3, queue_size=10)
 
 g_value = 9.81
 imu_scale_flag = 0
@@ -30,12 +41,13 @@ imu_res = np.iinfo(np.int16).max
 acc_scale = 0.00
 gyr_scale = 0.00
 
+
 def cb_SBP_MSG_POS_LLH(msg_llh, **source):
     # rate = rospy.Rate(10)
     msg = MsgPosLLH(msg_llh)
     # rospy.loginfo(msg)
 
-    llhCoods  = gps_loc()
+    llhCoods = gps_loc()
     llhCoods.loc.x = msg.lat
     llhCoods.loc.y = msg.lon
     llhCoods.loc.z = msg.height
@@ -46,6 +58,7 @@ def cb_SBP_MSG_POS_LLH(msg_llh, **source):
     # rospy.loginfo('lat lon message recieved')
     # # rospy.loginfo(llhCoods)
     # rate.sleep()
+
 
 def cb_SBP_MSG_BASELINE_NED(msg_ned, **source):
     # rate = rospy.Rate(10)
@@ -76,6 +89,7 @@ def cb_SBP_MSG_BASELINE_NED(msg_ned, **source):
     # # rospy.loginfo(rtk_fix)
     # rate.sleep()
 
+
 def cb_SBP_MSG_IMU_RAW(msg_imu_raw, **source):
     global imu_scale_flag
     global acc_scale
@@ -96,33 +110,38 @@ def cb_SBP_MSG_IMU_RAW(msg_imu_raw, **source):
     pub_imu_raw.publish(imu_raw_msg)
 
     if imu_scale_flag == 0:
-        imu_scale_flag = rospy.get_param(rospy.get_name()+'/imu_scale_flag')
+        imu_scale_flag = rospy.get_param(rospy.get_name() + "/imu_scale_flag")
         if imu_scale_flag == 1:
-            rospy.loginfo('IMU scale found')
-            acc_scale = rospy.get_param(rospy.get_name()+'/acc_scale')
-            rospy.loginfo('acc scale = ' + str(acc_scale))
-            rospy.loginfo('acc range = ' + str(rospy.get_param(rospy.get_name()+'/acc_range')))
-            gyr_scale = rospy.get_param(rospy.get_name()+'/gyr_scale')
-            rospy.loginfo('gyr scale = ' + str(gyr_scale))
-            rospy.loginfo('gyr range = ' + str(rospy.get_param(rospy.get_name()+'/gyr_range')))
+            rospy.loginfo("IMU scale found")
+            acc_scale = rospy.get_param(rospy.get_name() + "/acc_scale")
+            rospy.loginfo("acc scale = " + str(acc_scale))
+            rospy.loginfo(
+                "acc range = " + str(rospy.get_param(rospy.get_name() + "/acc_range"))
+            )
+            gyr_scale = rospy.get_param(rospy.get_name() + "/gyr_scale")
+            rospy.loginfo("gyr scale = " + str(gyr_scale))
+            rospy.loginfo(
+                "gyr range = " + str(rospy.get_param(rospy.get_name() + "/gyr_range"))
+            )
 
     imu_scaled_msg = imu_scaled()
     if imu_scale_flag == 1:
-        imu_scaled_msg.acc.x = imu_raw_msg.acc.x*acc_scale
-        imu_scaled_msg.acc.y = imu_raw_msg.acc.y*acc_scale
-        imu_scaled_msg.acc.z = imu_raw_msg.acc.z*acc_scale
-        imu_scaled_msg.gyr.x = imu_raw_msg.gyr.x*gyr_scale
-        imu_scaled_msg.gyr.y = imu_raw_msg.gyr.y*gyr_scale
-        imu_scaled_msg.gyr.z = imu_raw_msg.gyr.z*gyr_scale
+        imu_scaled_msg.acc.x = imu_raw_msg.acc.x * acc_scale
+        imu_scaled_msg.acc.y = imu_raw_msg.acc.y * acc_scale
+        imu_scaled_msg.acc.z = imu_raw_msg.acc.z * acc_scale
+        imu_scaled_msg.gyr.x = imu_raw_msg.gyr.x * gyr_scale
+        imu_scaled_msg.gyr.y = imu_raw_msg.gyr.y * gyr_scale
+        imu_scaled_msg.gyr.z = imu_raw_msg.gyr.z * gyr_scale
         imu_scaled_msg.tow = imu_raw_msg.tow
     else:
-        rospy.loginfo('IMU scale not available')
+        rospy.loginfo("IMU scale not available")
 
     pub_imu_scaled.publish(imu_scaled_msg)
 
     # rospy.loginfo('imu message recieved')
     # # rospy.loginfo(imu_raw_msg)
     # rate.sleep()
+
 
 def cb_SBP_MSG_IMU_AUX(msg_imu_aux, **source):
     # rate = rospy.Rate(100)
@@ -135,20 +154,20 @@ def cb_SBP_MSG_IMU_AUX(msg_imu_aux, **source):
     imu_aux_msg.imu_type = msg.imu_type
 
     acc_conf = msg.imu_conf & 7
-    acc_range = g_value*(2**(acc_conf+1))
-    acc_scale = acc_range/imu_res
+    acc_range = g_value * (2 ** (acc_conf + 1))
+    acc_scale = acc_range / imu_res
 
     gyr_conf = msg.imu_conf >> 4
-    gyr_range = 2000.00/(2**(gyr_conf))
-    gyr_scale = gyr_range/imu_res
+    gyr_range = 2000.00 / (2 ** (gyr_conf))
+    gyr_scale = gyr_range / imu_res
 
-    rospy.set_param(rospy.get_name()+'/imu_scale_flag',1)
-    rospy.set_param(rospy.get_name()+'/acc_conf',acc_conf)
-    rospy.set_param(rospy.get_name()+'/acc_range',acc_range)
-    rospy.set_param(rospy.get_name()+'/acc_scale',acc_scale)
-    rospy.set_param(rospy.get_name()+'/gyr_conf',gyr_conf)
-    rospy.set_param(rospy.get_name()+'/gyr_range',gyr_range)
-    rospy.set_param(rospy.get_name()+'/gyr_scale',gyr_scale)
+    rospy.set_param(rospy.get_name() + "/imu_scale_flag", 1)
+    rospy.set_param(rospy.get_name() + "/acc_conf", acc_conf)
+    rospy.set_param(rospy.get_name() + "/acc_range", acc_range)
+    rospy.set_param(rospy.get_name() + "/acc_scale", acc_scale)
+    rospy.set_param(rospy.get_name() + "/gyr_conf", gyr_conf)
+    rospy.set_param(rospy.get_name() + "/gyr_range", gyr_range)
+    rospy.set_param(rospy.get_name() + "/gyr_scale", gyr_scale)
 
     pub_imu_aux.publish(imu_aux_msg)
 
@@ -161,6 +180,7 @@ def cb_SBP_MSG_IMU_AUX(msg_imu_aux, **source):
     # rospy.loginfo(gyr_range)
     # rospy.loginfo(gyr_scale)
     # rate.sleep()
+
 
 def main():
     # parser = argparse.ArgumentParser()
@@ -176,21 +196,20 @@ def main():
 
     # threads = []
 
-
-    driver = PySerialDriver('/dev/ttyUSB0', baud=230400)
+    driver = PySerialDriver("/dev/ttyUSB0", baud=230400)
     framer = Framer(driver.read, None, verbose=True)
     source = Handler(framer)
 
-    rospy.set_param(rospy.get_name()+'/imu_scale_flag',0)
+    rospy.set_param(rospy.get_name() + "/imu_scale_flag", 0)
 
-
-    source.add_callback(cb_SBP_MSG_POS_LLH,msg_type=SBP_MSG_POS_LLH)
-    #source.add_callback(cb_SBP_MSG_BASELINE_NED,msg_type=SBP_MSG_BASELINE_NED)
-    #source.add_callback(cb_SBP_MSG_IMU_RAW,msg_type=SBP_MSG_IMU_RAW)
-    #source.add_callback(cb_SBP_MSG_IMU_AUX,msg_type=SBP_MSG_IMU_AUX)
+    source.add_callback(cb_SBP_MSG_POS_LLH, msg_type=SBP_MSG_POS_LLH)
+    # source.add_callback(cb_SBP_MSG_BASELINE_NED,msg_type=SBP_MSG_BASELINE_NED)
+    # source.add_callback(cb_SBP_MSG_IMU_RAW,msg_type=SBP_MSG_IMU_RAW)
+    # source.add_callback(cb_SBP_MSG_IMU_AUX,msg_type=SBP_MSG_IMU_AUX)
     source.start()
 
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
